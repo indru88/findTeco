57db5c6a68e52d9a2acc5996323e04ff8feace14
Fede Indrunas <findrunas@gmail.com>
2022-09-05 18:27:15 -0300

    query mongo mail

--- controllers/mail.js
+++ controllers/mail.js
@@ -11,17 +11,33 @@ const { handleHttpError } = require('../utils/handleErrors');
 const sendMail = async (req, res = response) => {
   try {
     const resultado = await Usuario.aggregate(
-      [
+      [{
+        $lookup:
         {
-          $lookup:
-          {
-            from: 'nombrednis',
-            localField: 'dnijefe',
-            foreignField: 'dni',
-            as: 'datosJefe'
-          }
+          from: 'nombrednis',
+          localField: 'dnijefe',
+          foreignField: 'dni',
+          as: 'datosjefe'
         }
-      ]
+      },
+      { $addFields: { nombJefe: { $first: "$datosjefe.nombre" }, apeJefe: { $first: "$datosjefe.apellido" } } },
+      {
+        $project: {
+          "Apellido y nombre": { $concat: ["$apellido", " ", "$nombre"] },
+          "Legajo": "$legajo",
+          "DNI": "$dni",
+          "Edad": { $dateDiff: { startDate: "$fechacumpleanos", endDate: "$$NOW", unit: "year" } },
+          "Rol": "$rol",
+          "Jefe Inmediato": {
+            $ifNull: [
+              { $concat: ["$nombJefe", " ", "$apeJefe"] },
+              { $concat: ["$apellido", " ", "$nombre"] }
+            ]
+          },
+          "Gerencia": "$gerencia",
+          "Sector": "$sector",
+        }
+      }]
     )
     res.json({ resultado })
     res.status(200)
