3a2477c83a7f8a7a3da4690a39a1612dfad961ba
Fede Indrunas <findrunas@gmail.com>
2022-09-06 02:46:59 -0300

    fix fechas

--- utils/excel.js
+++ utils/excel.js
@@ -2,7 +2,7 @@ const xlsx = require('xlsx');
 const jcc = require('json-case-convertor');
 
 cargarExcel = () => {
-  const workbook = xlsx.readFile('/usr/src/app/utils/Nomina.xlsx')
+  const workbook = xlsx.readFile('/usr/src/app/utils/Nomina.xlsx', { type: 'binary', cellDates: true, dateNF: 'dd/mm/yyyy', strip: false, blankrows: true })
   const workbookSheets = workbook.SheetNames
   const sheet = workbookSheets[0]
   const dataExcel = xlsx.utils.sheet_to_json(workbook.Sheets[sheet])
